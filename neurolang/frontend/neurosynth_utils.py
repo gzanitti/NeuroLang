ab39f65039a6719bd46235e8d582cc7ef1e2b9a5
gzanitti <gzanitti@gmail.com>
2020-04-23 12:38:42 +0200

    Manual merge: Neurosynth utils with basic logic

--- neurolang/frontend/neurosynth_utils.py
+++ neurolang/frontend/neurosynth_utils.py
@@ -1,7 +1,10 @@
 import logging
 import os
-from ..regions import region_set_from_masked_data
+
 from pkg_resources import resource_exists, resource_filename
+
+from ..regions import region_set_from_masked_data
+
 try:
     import neurosynth as ns
 except ModuleNotFoundError:
@@ -13,19 +16,28 @@ class NeuroSynthHandler(object):
         self._dataset = ns_dataset
 
     def ns_region_set_from_term(
-        self, terms, frequency_threshold=0.05, q=0.01,
-        prior=0.5, image_type=None
+        self,
+        terms,
+        frequency_threshold=0.05,
+        q=0.01,
+        prior=0.5,
+        image_type=None,
     ):
 
         if image_type is None:
-            image_type = f'association-test_z_FDR_{q}'
+            image_type = f"association-test_z_FDR_{q}"
 
         if self._dataset is None:
             dataset = self.ns_load_dataset()
             self._dataset = dataset
-        studies_ids = self._dataset.get_studies(
-            features=terms, frequency_threshold=frequency_threshold
-        )
+        if isinstance(terms, list):
+            studies_ids = self._dataset.get_studies(
+                features=terms, frequency_threshold=frequency_threshold
+            )
+        else:
+            studies_ids = self._dataset.get_studies(
+                expression=terms, frequency_threshold=frequency_threshold
+            )
         ma = ns.meta.MetaAnalysis(self._dataset, studies_ids, q=q, prior=prior)
         data = ma.images[image_type]
         masked_data = self._dataset.masker.unmask(data)
@@ -36,16 +48,18 @@ class NeuroSynthHandler(object):
 
     def ns_load_dataset(self):
 
-        if resource_exists('neurolang.frontend',
-                           'neurosynth_data/dataset.pkl'):
-            file = resource_filename('neurolang.frontend',
-                                     'neurosynth_data/dataset.pkl')
+        if resource_exists(
+            "neurolang.frontend", "neurosynth_data/dataset.pkl"
+        ):
+            file = resource_filename(
+                "neurolang.frontend", "neurosynth_data/dataset.pkl"
+            )
             dataset = ns.Dataset.load(file)
         else:
-            path = resource_filename('neurolang.frontend', 'neurosynth_data')
+            path = resource_filename("neurolang.frontend", "neurosynth_data")
             logging.info(
-                f'Downloading neurosynth database'
-                f' and features in path: {path}'
+                f"Downloading neurosynth database"
+                f" and features in path: {path}"
             )
             dataset = self.download_ns_dataset(path)
 
@@ -56,7 +70,7 @@ class NeuroSynthHandler(object):
         if not os.path.exists(path):
             os.makedirs(path)
         ns.dataset.download(path=path, unpack=True)
-        dataset = ns.Dataset(os.path.join(path, 'database.txt'))
-        dataset.add_features(os.path.join(path, 'features.txt'))
-        dataset.save(os.path.join(path, 'dataset.pkl'))
+        dataset = ns.Dataset(os.path.join(path, "database.txt"))
+        dataset.add_features(os.path.join(path, "features.txt"))
+        dataset.save(os.path.join(path, "dataset.pkl"))
         return dataset
