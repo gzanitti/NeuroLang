efeacbe6bea75376dd6eefb8e01e466fc6807437
gzanitti <gzanitti@gmail.com>
2020-04-23 13:46:59 +0200

    Manual merge: missed file

--- neurolang/datalog/constraints_representation.py
+++ neurolang/datalog/constraints_representation.py
@@ -6,18 +6,37 @@ the extensional, intensional, and builtin
 sets and has support for constraints.
 """
 
+from ..logic import LogicOperator
 from .basic_representation import DatalogProgram
 
 
-class DatalogConstraintsProgramMixin():
+class RightImplication(LogicOperator):
+    """
+    This class defines implications to the right. They are used to define
+    constraints in datalog programs. The functionality is the same as
+    that of an implication, but with body and head inverted in position.
+    """
 
-    protected_keywords = set({'__constraints__'})
+    def __init__(self, antecedent, consequent):
+        self.antecedent = antecedent
+        self.consequent = consequent
+        self._symbols = consequent._symbols | antecedent._symbols
 
-    def load_constraints(self, expression_block):
-        self.symbol_table['__constraints__'] = expression_block
+    def __repr__(self):
+        return "RightImplication{{{} \u2192 {}}}".format(
+            repr(self.antecedent), repr(self.consequent)
+        )
 
-    def get_constraints(self):
-        return self.symbol_table['__constraints__']
+
+class DatalogConstraintsProgramMixin:
+
+    protected_keywords = set({"__constraints__"})
+
+    def load_constraints(self, union_of_constraints):
+        self.symbol_table["__constraints__"] = union_of_constraints
+
+    def constraints(self):
+        return self.symbol_table["__constraints__"]
 
 
 class DatalogConstraintsProgram(
